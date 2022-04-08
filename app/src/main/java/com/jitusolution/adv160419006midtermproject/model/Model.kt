407bd51afc8f5dd37bd7cd2eeecc27a25f16ed10
s160419006 <s160419006@student.ubaya.ac.id>
2022-04-08 10:02:35 +0700

    change data

--- app/src/main/java/com/jitusolution/adv160419006midtermproject/model/Model.kt
+++ app/src/main/java/com/jitusolution/adv160419006midtermproject/model/Model.kt
@@ -4,10 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 data class Kuliner(
     val id:String?,
-    @SerializedName("resto_name")
     val name:String?,
     val address:String?,
-    @SerializedName("favorit_food")
     val favorit:String?,
     val phone:String?
 )
